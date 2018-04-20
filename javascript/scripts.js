5206edc822835f8d62951dfaeffa1086d08489ff
yye <lilac.yyt@gmail.com>
2018-04-20 03:29:19 -0400

    change all file names as foldername.extension

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -7,7 +7,6 @@ function changeDirNames() {
 	// Make all directory names in the correct format.
 	for (i = 1; i < dir_list.length; i++) {
 		var string = dir_list[i].toLowerCase().replace('../stories/', '').replace(/ /g, '-').replace(/[^\w\s-]/g, '');
-		console.log(string)
 
 		// Rename story folder names.
 		fs.rename(dir_list[i], '../stories/' + string, function (err) {
@@ -16,4 +15,33 @@ function changeDirNames() {
 		});
 	}
 }
+
+function changeFileNames() {
+	var dir_list = utils.getDirectoriesRecursive('../stories')
+	for (i = 1; i < dir_list.length; i++) {
+		var string = dir_list[i] + "/"
+		// Get all file names under each folder
+		var file_list = fs.readdirSync(string)
+
+		for (j = 0; j < file_list.length; j++) {
+			// Get the extension of each file
+			var extension = getFileExtension(file_list[j]).toLowerCase()
+			// Get the pure folder name
+			newname = string.replace('../stories/', '').replace('/', '')
+			
+			// Rename each file name in the folder as foldername.extension
+			fs.rename(string + file_list[j], string + newname + '.' + extension, function (err) {
+				if (err) throw err;
+				console.log('renamed file complete')
+			})
+		}
+	}
+}
+
+function getFileExtension(filename) {
+	return filename.split('.').pop();
+}
+
 changeDirNames()
+
+changeFileNames()
