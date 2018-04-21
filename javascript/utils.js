7de6a392a01412c9ecde6d700cbeec921ce22079
yye <lilac.yyt@gmail.com>
2018-04-20 20:17:52 -0400

    fix staff database: midle name and other language

--- javascript/utils.js
+++ javascript/utils.js
@@ -111,7 +111,7 @@ function readFromCsvAndPopulateStaffSchema() {
 }
 
 // readFromCsvAndPopulateStaffSchema()
-readFromCsvAndPopulateStorySchema()
+// readFromCsvAndPopulateStorySchema()
 
 // ############################################################
 exports.getDirectoriesRecursive = getDirectoriesRecursive
